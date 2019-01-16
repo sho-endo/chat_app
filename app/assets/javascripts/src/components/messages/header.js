4e44a160952b5c5ac086023c641c5e613f2f3724
sho-endo <s.endo.8951@gmail.com>
2019-01-17 08:51:37 +0900

    Create header

--- app/assets/javascripts/src/components/messages/header.js
+++ app/assets/javascripts/src/components/messages/header.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 class Header extends React.Component {
   render() {
+    const userName = document.getElementById('user-name').getAttribute('data')
+    const userId = document.getElementById('user-id').getAttribute('data')
     return (
         <header className='header'>
           <div className='header-left'>
@@ -13,9 +15,9 @@ class Header extends React.Component {
             <ul className='nav navbar-nav'>
               <li className='serch-user-btn'><a href='#'>ユーザーを探す</a></li>
               <li className='dropdown header-dropdown'>
-                <a href='#' className='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>ログイン中のユーザー名<span className='caret'></span></a>
+                <a href='#' className='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>{userName}<span className='caret'></span></a>
                 <ul className='dropdown-menu user-menu'>
-                  <li className='user-menu-list'><a href='#'>ログインユーザーのマイページ</a></li>
+                  <li className='user-menu-list'><a href={'/users/' + userId}>マイページ</a></li>
                   <li className='user-menu-list'><a href='/users/sign_out' data-method='delete'>ログアウト</a></li>
                 </ul>
               </li>
