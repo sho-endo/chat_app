62572d4e07caec4218ac402d605fd5ba06a8ca41
sho-endo <s.endo.8951@gmail.com>
2019-01-27 21:01:40 +0900

    Forbid make friends with oneself

--- app/assets/javascripts/src/components/users/serchedUserList.js
+++ app/assets/javascripts/src/components/users/serchedUserList.js
@@ -4,17 +4,20 @@ import _ from 'lodash'
 
 function SerchedUserList(props) {
   const serchedUsers = props.serchedUsers
+  const currentUserId = Number(document.getElementById('current_user-id').getAttribute('data'))
   return (
     <ul className='serch_user_list'>
       {
         _.map(serchedUsers, function(user) {
-          return (
-          <li key={user.id} className='serch_user_list_item'>
-            <div className='serch_user_list_result' onClick={props.onClick} value={user.id}>
-              <span>{user.name}</span>
-            </div>
-          </li>
-          )
+          if (user.id !== currentUserId) {
+            return (
+              <li key={user.id} className='serch_user_list_item'>
+                <div className='serch_user_list_result' onClick={props.onClick} value={user.id}>
+                  <span>{user.name}</span>
+                </div>
+              </li>
+            )
+          }
         })
       }
     </ul>
--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -3,12 +3,18 @@ class Friendship < ActiveRecord::Base
   belongs_to :to_user, class_name: 'User'
   validates :from_user_id, presence: true, uniqueness: { scope: :to_user_id }
   validates :to_user_id, presence: true
-  validate :combination_of_user_id_must_be_unique
+  validate :combination_of_user_id_must_be_unique, :cannot_make_friends_with_oneself
 
   private
     def combination_of_user_id_must_be_unique
       if Friendship.find_by(from_user_id: to_user_id, to_user_id: from_user_id)
-        errors.add(:from_user_id, "すでに友達です")
+        errors.add(:base, "すでに友達です")
+      end
+    end
+
+    def cannot_make_friends_with_oneself
+      if from_user_id == to_user_id
+        errors.add(:base, "自分とはチャットできません")
       end
     end
 end
